e75fa0f68da5de2aa3a29eb53c9e01b9461692ae
Wojciech Antonik <wojtekantonik9@gmail.com>
2022-01-15 16:32:30 +0100

    ts and scripts setting

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json({}));
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
-  console.log("Server is running");
+  console.log(`Server is running on port ${PORT}`);
 
   connection();
   app.use(router);
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 5000;
 export const MONGODB_URI = process.env.MONGODB_URI || "";
